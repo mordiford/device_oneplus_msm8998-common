1d0f85a5831031d0c68cbc096212be1dee3ae905
lindwurm <lindwurm.q@gmail.com>
2018-08-12 20:36:15 +0900

    msm8998: remove KBUILD_COMPILER_STRING overrides
    not needed with Singularity

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,8 +76,6 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/Singularity
 TARGET_KERNEL_CONFIG := singularity_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := 7.0.2
-KBUILD_COMPILER_STRING := Android (4751641 based on r328903) clang version 7.0.2 (based on LLVM 7.0.2svn)
-export KBUILD_COMPILER_STRING
 
 # Enable real time lockscreen charging current values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
